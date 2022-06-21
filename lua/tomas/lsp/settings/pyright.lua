e93aa74c00044e7be50c38fd0427f1fd0bb7cd5d
Tomas Gareau <tggareau@gmail.com>
2022-06-21 15:21:56 -0600

    feat: default reportUnknownMemberType to none for Pyright
    Numpy's types are mostly unknown, making Pyright cry

--- lua/tomas/lsp/settings/pyright.lua
+++ lua/tomas/lsp/settings/pyright.lua
@@ -7,6 +7,7 @@ return {
 				typeCheckingMode = "basic",
 				diagnosticSeverityOverrides = {
 					reportUnknownParameterType = "none",
+          reportUnknownMemberType = "none",
 				},
 			},
 		},
