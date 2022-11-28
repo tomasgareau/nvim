e44109cbf33fc38f20c5f7a0275cd5ba221694af
Tomas Gareau <tggareau@gmail.com>
2022-11-28 13:43:54 -0600

    chore: update LSP handler setup for 0.8

--- lua/tomas/lsp/handlers.lua
+++ lua/tomas/lsp/handlers.lua
@@ -46,7 +46,7 @@ end
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.document_highlight then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -66,9 +66,9 @@ if not keymaps_ok then
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" or client.name == "gopls" then
+	if client.name == "tsserver" or client.name == "gopls" or client.name == "terraformls" then
     -- disable LSP formatting here if it doesn't work (we can use null-ls instead)
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
   if keymaps_ok then
     keymaps.lsp_keymaps(bufnr)
@@ -76,13 +76,11 @@ M.on_attach = function(client, bufnr)
 	lsp_highlight_document(client)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
 	return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
