4ec36e6c8930dfff15b1b3cd6557903060a6d858
Tomas Gareau <tggareau@gmail.com>
2023-06-12 10:37:36 -0600

    feat: use stylua for formatting lua
    This matches the pre-commit in this repo

---
+++ lua/user/null-ls.lua
@@ -0,0 +1,27 @@
+local M = {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = "BufReadPre",
+  commit = "a138b14099e9623832027ea12b4631ddd2a49256",
+  dependencies = {
+    {
+      "nvim-lua/plenary.nvim",
+      commit = "36aaceb6e93addd20b1b18f94d86aecc552f30c4",
+      lazy = true,
+    },
+  },
+}
+
+function M.config()
+  local null_ls = require "null-ls"
+  local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
+
+  null_ls.setup {
+    debug = false,
+    sources = {
+      formatting.stylua,
+    },
+  }
+end
+
+return M
