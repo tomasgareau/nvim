6cb07e1d4fa010134251b26ab69d44fda83bbf26
Tomas Gareau <tggareau@gmail.com>
2023-06-19 09:16:39 -0600

    feat: tweak kanagawa light theme to be less yellow

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -29,19 +29,35 @@ function M.config()
 		transparent = false, -- do not set background color
 		dimInactive = true, -- dim inactive window `:h hl-NormalNC`
 		terminalColors = true, -- define vim.g.terminal_color_{0,17}
-		colors = { -- add/modify theme and palette colors
-			palette = {},
-			theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+		colors = {
+			-- add/modify theme and palette colors
+			palette = {
+				lotusWhite0 = "#f7f1e1",
+				lotusWhite1 = "#f6efdd",
+				lotusWhite2 = "#f5edd9",
+				lotusWhite3 = "#f3ebd4",
+				lotusWhite4 = "#f2e9d0",
+				lotusWhite5 = "#f1e7cc",
+			},
+			theme = {
+				wave = {},
+				lotus = {},
+				dragon = {},
+				all = {},
+			},
 		},
 		overrides = function(colors) -- add/modify highlights
 			return {}
 		end,
 		theme = "wave", -- Load "wave" theme when 'background' option is not set
-		background = { -- map the value of 'background' option to a theme
+		background = {
+			-- map the value of 'background' option to a theme
 			dark = "wave", -- try "dragon" !
 			light = "lotus",
 		},
 	})
+
+	vim.cmd("colorscheme kanagawa")
 end
 
 return M
