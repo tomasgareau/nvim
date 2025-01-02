b207f781c36e780ce58d40619e5041b470179d47
Tomas Gareau <tggareau@gmail.com>
2025-01-02 09:46:32 -0600

    Add tabstop options on top of kickstart

--- lua/options.lua
+++ lua/options.lua
@@ -61,4 +61,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Set tab/shift widths
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 -- vim: ts=2 sts=2 sw=2 et
