a70202e3e4ee6bdca8c5fcf727dc10ba5cabc79b
Tomas Gareau <tggareau@gmail.com>
2025-04-25 10:59:05 -0600

    Add ruff_fix, ruff_format to conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				python = { "ruff_fix", "ruff_format" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
