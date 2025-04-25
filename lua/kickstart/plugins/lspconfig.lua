cc0a3bb59570e77bb5e2dbd74064e8a67d7692f9
Tomas Gareau <tggareau@gmail.com>
2025-04-25 10:59:05 -0600

    Show signature help with <C-s>

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -152,6 +152,9 @@ return {
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+					-- Show signature help in insert mode
+					map("<C-s>", vim.lsp.buf.signature_help, "[S]ignature Help", { "i" })
+
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
